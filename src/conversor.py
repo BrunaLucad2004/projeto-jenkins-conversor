430dc54749a2575f782f901e9f33bafb7479f1c2
Bruna Lucas <jsantos.bu26@gmail.com>
2026-05-25 14:24:40 -0400

    Cenario 3

--- src/conversor.py
+++ src/conversor.py
@@ -1,10 +1,10 @@
-def fahrenheit_para_celsius(fahrenheit)
+def fahrenheit_para_celsius(fahrenheit):
     """Converte uma temperatura de Fahrenheit para Celsius."""
-    celsius = (fahrenheit - 32) * 5 / 9
+    celsius = (fahrenheit - 32) * 5 / 8
     return round(celsius, 2)
 
 
-def celsius_para_fahrenheit(celsius)
+def celsius_para_fahrenheit(celsius):
     """Converte uma temperatura de Celsius para Fahrenheit."""
     fahrenheit = (celsius * 9 / 5) + 32
     return round(fahrenheit, 2)
